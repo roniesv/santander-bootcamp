adca2e7d95bb0bc5017ed8253225151fb7d5585e
ronsv <ronie.souzavieira@gmail.com>
2021-06-25 19:35:54 -0300

    exercicio00 media_aritmetica

---
+++ logica/aula00.java
@@ -0,0 +1,24 @@
+public class Aula00 {
+
+	/*  Conceitos: lógica, programação, abstração---> resolver problemas. */
+	public static void main (String []args){
+	   //Situação problema: Calcular média notas 4 bimestre
+	  
+		double media;
+	       	double n1,n2,n3,n4;
+
+		n1=5;
+		n2=7;
+		n3=10;
+		n4=3;
+		media = (n1+n2+n3+n4)/4;
+
+		System.out.println("A média é: "+media);
+	  
+
+
+	
+
+	}
+
+}
