f19158ab91df785ff32fe8e8d3992292f677775d
ronsv <ronie.souzavieira@gmail.com>
2021-08-26 22:13:10 -0300

    profile com arquivos em frm diferentes, command line x, variavel de ambiente,

--- springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
+++ springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
@@ -8,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @Value("${app.message}")
     private String appMessage;
+
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnvironmentVariable;
+
     @GetMapping("/")
     public String getAppMessage(){
         return appMessage;
+    }
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable(){
+        return "A seguinte variavel de ambiente foi passada: "+dbEnvironmentVariable;
     }
 }
