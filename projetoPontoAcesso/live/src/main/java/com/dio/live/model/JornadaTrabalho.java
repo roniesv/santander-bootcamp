ae9886ad259be597ba95b3a8a7a7f3b99dac538d
ronsv <ronie.souzavieira@gmail.com>
2021-09-07 00:41:13 -0300

    implemetacao model

---
+++ projetoPontoAcesso/live/src/main/java/com/dio/live/model/JornadaTrabalho.java
@@ -0,0 +1,19 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class JornadaTrabalho {
+    @Id
+    private long id;
+    private String descricao;
+}
---
+++ projetoPontoAcesso/live/src/main/java/com/dio/live/model/Localidade.java
@@ -0,0 +1,20 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class Localidade {
+    @Id
+    private long id;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    private String descricao;
+}
